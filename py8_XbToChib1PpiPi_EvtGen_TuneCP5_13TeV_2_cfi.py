e5a335524f6537f8386c1af0b8aa6d68db3dac29
CesarMH18 <cesarmh18@gmail.com>
2018-06-12 15:54:01 -0500

    Update py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_2_cfi.py

--- py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_2_cfi.py
+++ py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_2_cfi.py
@@ -6,23 +6,10 @@ from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
-    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
-    maxEventsToPrint = cms.untracked.int32(0),
+    maxEventsToPrint = cms.untracked.int32(3),
     comEnergy = cms.double(13000.0),
-    ExternalDecays = cms.PSet(
-        EvtGen130 = cms.untracked.PSet(
-            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
-            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
-            list_forced_decays = cms.vstring('myX_b'),        # will force
-            operates_on_particles = cms.vint32(10551),        # chi_b0 id=10551 we care just about our signal particles
-            convertPythiaCodes = cms.untracked.bool(False),
-            user_decay_embedded= cms.vstring(
-
-            )
-	),
-        parameterSets = cms.vstring('EvtGen130')
-    ),
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
 	pythia8CP5SettingsBlock,
