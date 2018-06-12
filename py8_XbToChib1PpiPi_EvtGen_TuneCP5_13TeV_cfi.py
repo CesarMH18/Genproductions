073fee754ae601a953ca75ca98d5b5bd5f9e8b47
CesarMH18 <cesarmh18@gmail.com>
2018-06-12 14:38:33 -0500

    Update py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_cfi.py

--- py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_cfi.py
+++ py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_cfi.py
@@ -1,4 +1,4 @@
-# cfg file for X_b -> Chib(1P) pion+ pion-,  we are neglecting Chi_b0. Masses and widths are matched between pythia, evtgen and PDG 2016
+# cfg file for X_b -> Chib(1P) pion+ pion-. Masses and widths are matched between pythia, evtgen and PDG 2016
 #
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
@@ -13,28 +13,28 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
         EvtGen130 = cms.untracked.PSet(
             decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
             particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
-            list_forced_decays = cms.vstring('myX_b','mychi_b1','mychi_b2'),        # will force one at the time
-            operates_on_particles = cms.vint32(20553,555),                  # we care just about our signal particles
+            list_forced_decays = cms.vstring('myX_b'),        # will force one at the time
+            operates_on_particles = cms.vint32(10551),                  # we care just about our signal particles
             convertPythiaCodes = cms.untracked.bool(False),
             user_decay_embedded= cms.vstring(
 """
 Particle Upsilon 9.4603000 0.00005402
-Particle chi_b1 10.25546 0.00000
-Particle chi_b2 10.26865 0.00000
+Particle chi_b1  9.8927800 0.00000
+Particle chi_b0  10.500000 0.00000
+
 Alias myUpsilon Upsilon
 Alias mychi_b1 chi_b1
-Alias mychi_b2 chi_b2
-Alias myX_b 
+Alias myX_b    chi_b0 
+
 Decay myUpsilon
 1.0   mu+  mu-          PHOTOS  VLL;
 Enddecay
 Decay mychi_b1
 1.0   gamma  myUpsilon  HELAMP 1. 0. 1. 0. -1. 0. -1. 0.;
 Enddecay
-Decay mychi_b2
-1.0   gamma  myUpsilon  PHSP;
+Decay myX_b
+1.0   mychi_b1 pi+ pi-  PHSP ;
 Enddecay
-End
 """
             )
 	),
@@ -44,20 +44,18 @@ End
         pythia8CommonSettingsBlock,
 	pythia8CP5SettingsBlock,
         processParameters = cms.vstring(
-            'Bottomonium:states(3PJ) = 20553,555',
-            'Bottomonium:O(3PJ)[3P0(1)] = 0.085,0.085',
-            'Bottomonium:O(3PJ)[3S1(8)] = 0.04,0.04',
-            'Bottomonium:gg2bbbar(3PJ)[3PJ(1)]g = on,on',
-            'Bottomonium:qg2bbbar(3PJ)[3PJ(1)]q = on,on',
-            'Bottomonium:qqbar2bbbar(3PJ)[3PJ(1)]g = on,on',
-            'Bottomonium:gg2bbbar(3PJ)[3S1(8)]g = on,on',
-            'Bottomonium:qg2bbbar(3PJ)[3S1(8)]q = on,on',
-            'Bottomonium:qqbar2bbbar(3PJ)[3S1(8)]g = on,on',
+            'Bottomonium:states(3PJ) = 10551',      #We get chib0(1P) to simulate X_b
+            'Bottomonium:O(3PJ)[3P0(1)] = 0.085',
+            'Bottomonium:O(3PJ)[3S1(8)] = 0.04',
+            'Bottomonium:gg2bbbar(3PJ)[3PJ(1)]g = on',
+            'Bottomonium:qg2bbbar(3PJ)[3PJ(1)]q = on',
+            'Bottomonium:qqbar2bbbar(3PJ)[3PJ(1)]g = on',
+            'Bottomonium:gg2bbbar(3PJ)[3S1(8)]g = on',
+            'Bottomonium:qg2bbbar(3PJ)[3S1(8)]q = on',
+            'Bottomonium:qqbar2bbbar(3PJ)[3S1(8)]g = on',
             'PhaseSpace:pTHatMin = 2.',
-            '20553:m0 = 10.25546',
-            '555:m0 = 10.26865',
-            '20553:onMode = off',
-            '555:onMode = off'
+            '10551:m0 = 10.500000',        # mass chib0(1P)=9.85944, we use another value to simulate X_b
+            '10551:onMode = off',
             ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CP5Settings',
@@ -66,10 +64,10 @@ End
     )
 )
 
-# We will filter for chi_b1, and chi_b2, first on the ID, then in the mass, this will constraint the photon daughter
+# We will filter for chi_b1
 
 pwaveIDfilter = cms.EDFilter("MCSingleParticleFilter",
-    ParticleID = cms.untracked.vint32(20553,555),
+    ParticleID = cms.untracked.vint32(20553),
     MinPt = cms.untracked.vdouble(0.0, 0.0),
     MinEta = cms.untracked.vdouble(-9., -9.),
     MaxEta = cms.untracked.vdouble(9., 9.),
@@ -85,8 +83,8 @@ pwaveMassfilter = cms.EDFilter("MCParticlePairFilter",
     MinP = cms.untracked.vdouble(0.,0.),
     ParticleID1 = cms.untracked.vint32(553),
     ParticleID2 = cms.untracked.vint32(22),
-    MinInvMass = cms.untracked.double(10.25),
-    MaxInvMass = cms.untracked.double(10.27),
+    MinInvMass = cms.untracked.double(9.88),
+    MaxInvMass = cms.untracked.double(9.91),
 )
 
 # Next two muon filter are derived from muon reconstruction
