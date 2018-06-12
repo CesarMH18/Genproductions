f88c83ce9815a28c3809bd4a2d59194d51deb7d0
CesarMH18 <cesarmh18@gmail.com>
2018-06-12 15:35:58 -0500

    Update py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_2_cfi.py

--- py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_2_cfi.py
+++ py8_XbToChib1PpiPi_EvtGen_TuneCP5_13TeV_2_cfi.py
@@ -1,25 +1,32 @@
+#cfg file for X_b -> Chib(1P) pion+ pion-. Masses and widths are matched between pythia, evtgen and PDG 2016
+#The mass of the chi_b0(1P) is set to 10.5 GeV (desired resonant mass)
+
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
-
-etafilter = cms.EDFilter("PythiaFilter",
-        MaxEta = cms.untracked.double(9999.0),
-        MinEta = cms.untracked.double(-9999.0),
-        ParticleID = cms.untracked.int32(10551)
-)
-
-
 generator = cms.EDFilter("Pythia8GeneratorFilter",
-        pythiaHepMCVerbosity = cms.untracked.bool(False),
-        maxEventsToPrint = cms.untracked.int32(3),
-        pythiaPylistVerbosity = cms.untracked.int32(1),
-        displayPythiaCards = cms.untracked.bool(False),
-        comEnergy = cms.double(13000.0),
-        PythiaParameters = cms.PSet(
-                pythia8CommonSettingsBlock,
-                pythia8CP5SettingsBlock,
-                pythiaEtab = cms.vstring(
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    comEnergy = cms.double(13000.0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
+            list_forced_decays = cms.vstring('myX_b'),        # will force
+            operates_on_particles = cms.vint32(10551),        # chi_b0 id=10551 we care just about our signal particles
+            convertPythiaCodes = cms.untracked.bool(False),
+            user_decay_embedded= cms.vstring(
+
+            )
+	),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+	pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
                        'Bottomonium:all=on',
                        '10551:addChannel 1 1.00 100 211 -211 20553',
                        '10551:onMode = off',
@@ -30,14 +37,60 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
                        '553:onIfMatch 13 -13',
                        '10551:m0=10.5',
                        '10551:mWidth =0.00'
-),
-                parameterSets = cms.vstring(
-                        'pythia8CommonSettings',
-                        'pythia8CP5Settings',
-                        'pythiaEtab',
-                )
-        )
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    )
 )
 
+# We will filter for chi_b1
 
-ProductionFilterSequence = cms.Sequence(generator+etafilter)
+pwaveIDfilter = cms.EDFilter("MCSingleParticleFilter",
+    ParticleID = cms.untracked.vint32(20553),   #id chi_b1(IP) =20553
+    MinPt = cms.untracked.vdouble(0.0, 0.0),
+    MinEta = cms.untracked.vdouble(-9., -9.),
+    MaxEta = cms.untracked.vdouble(9., 9.),
+    Status = cms.untracked.vint32(2, 2)
+)
+
+pwaveMassfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(2, 1),
+    MinPt = cms.untracked.vdouble(7.9, 0.2),
+    MaxEta = cms.untracked.vdouble(1.6, 1.6),
+    MinEta = cms.untracked.vdouble(-1.6, -1.6),
+    ParticleCharge = cms.untracked.int32(0),
+    MinP = cms.untracked.vdouble(0.,0.),
+    ParticleID1 = cms.untracked.vint32(553),
+    ParticleID2 = cms.untracked.vint32(22),
+    MinInvMass = cms.untracked.double(9.88),
+    MaxInvMass = cms.untracked.double(9.91),
+)
+
+# Next two muon filter are derived from muon reconstruction
+
+muminusfilter = cms.EDFilter("PythiaDauVFilter",
+    MotherID = cms.untracked.int32(0),
+    MinPt = cms.untracked.vdouble(2.5, 2.5, 3.5),
+    ParticleID = cms.untracked.int32(553),
+    ChargeConjugation = cms.untracked.bool(False),
+    MinEta = cms.untracked.vdouble(1.2, -1.6, -1.2),
+    MaxEta = cms.untracked.vdouble(1.6, -1.2, 1.2),
+    NumberDaughters = cms.untracked.int32(1),
+    DaughterIDs = cms.untracked.vint32(-13, -13, -13)
+)
+
+muplusfilter = cms.EDFilter("PythiaDauVFilter",
+    MotherID = cms.untracked.int32(0),
+    MinPt = cms.untracked.vdouble(2.5, 2.5, 3.5),
+    ParticleID = cms.untracked.int32(553),
+    ChargeConjugation = cms.untracked.bool(False),
+    MinEta = cms.untracked.vdouble(1.2, -1.6, -1.2),
+    MaxEta = cms.untracked.vdouble(1.6, -1.2, 1.2),
+    NumberDaughters = cms.untracked.int32(1),
+    DaughterIDs = cms.untracked.vint32(13, 13, 13)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*pwaveIDfilter*pwaveMassfilter*muminusfilter*muplusfilter)
+
